c8efd59654d3998448e8a119cc5857fd77a8068f
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 18:06:06 +0700

    Update webhook_handler.py

--- webhook_handler.py
+++ webhook_handler.py
@@ -1,6 +1,5 @@
 import requests
 
-# Твой токен Telegram-бота
 TOKEN = '7179411906:AAHtkezX3Ng0ko_H8MafcCwHNYQrjunmiv4'
 
 def handle_update(data):
@@ -10,11 +9,28 @@ def handle_update(data):
         chat_id = data["message"]["chat"]["id"]
         text = data["message"].get("text", "")
 
-        # Ответ пользователю
-        reply = "Принято!" if text else "Не понял тебя 🤖"
+        if text == "/start":
+            reply = "Добро пожаловать в Functional Body Bot 👊"
+            keyboard = {
+                "keyboard": [[{"text": "Мой рацион"}, {"text": "Моя тренировка"}]],
+                "resize_keyboard": True
+            }
+            send_message(chat_id, reply, keyboard)
+        else:
+            reply = "Принято!"
+            send_message(chat_id, reply)
 
-        # Отправка ответа через Telegram API
-        requests.post(
-            f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-            json={"chat_id": chat_id, "text": reply}
-        )
+
+def send_message(chat_id, text, reply_markup=None):
+    payload = {
+        "chat_id": chat_id,
+        "text": text
+    }
+
+    if reply_markup:
+        payload["reply_markup"] = reply_markup
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        json=payload
+    )
