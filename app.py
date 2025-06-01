6317b73205130a12ff84f14cc2a383c810da1cd8
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 18:48:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, request
-from webhook_handler import handle_update
+from webhook_handler import handle_update, TOKEN
 
 app = Flask(__name__)
 
-@app.route(f"/{TOKEN}", methods=["POST"])
+@app.route('/')
+def index():
+    return 'Бот работает!'
+
+@app.route(f'/{TOKEN}', methods=["POST"])
 def webhook():
     data = request.get_json()
     handle_update(data)
-    return "ok", 200
-
-@app.route("/", methods=["GET"])
-def index():
-    return "Бот работает", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    return '', 200
