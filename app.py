66d3641dfc217c8ae7fa5540b9127b3c2ff24372
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 19:21:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from webhook_handler import handle_update, TOKEN
+import os
 
 app = Flask(__name__)
 
@@ -12,3 +13,7 @@ def webhook():
     data = request.get_json()
     handle_update(data)
     return '', 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
