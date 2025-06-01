23f5a32b4504f181498832786e64800c49258662
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 23:09:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ def webhook():
         data = request.get_json()
         handle_update(data)
         return 'OK'
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
