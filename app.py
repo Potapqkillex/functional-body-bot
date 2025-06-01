d0054150fee18da707d823afeb0d2e84f77fbc18
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 19:54:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from webhook_handler import handle_update
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Bot is running."
-
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     handle_update(data)
     return '', 200
+
+if __name__ == '__main__':
+    app.run()
