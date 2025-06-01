b09b080e8ae09e267d6209cc2cc2009509a51f1b
Potapqkillex <potapqkillex@ya.ru>
2025-06-01 17:42:49 +0700

    Update webhook_handler.py

--- webhook_handler.py
+++ webhook_handler.py
@@ -1,3 +1,12 @@
 def handle_update(data):
     print("Handling update:", data)
-    # Тут позже добавим обработку команд и сообщений
+
+    message = data.get('message')
+    if not message:
+        return
+
+    chat_id = message['chat']['id']
+    text = message.get('text', '')
+
+    # здесь можно добавить любую логику
+    print(f"Сообщение от {chat_id}: {text}")
